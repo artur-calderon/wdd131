1bfb261ab36b23edee0a463c0c967fac0bbb63df
artur-calderon <artur.calderonart@gmail.com>
2025-11-15 14:17:53 -0400

    whater page

---
+++ scripts/place.js
@@ -0,0 +1,28 @@
+const yearElement = document.getElementById("currentyear");
+const currentYear = new Date().getFullYear();
+yearElement.textContent = currentYear;
+
+const modifiedElement = document.getElementById("lastmodified");
+modifiedElement.textContent = document.lastModified;
+
+const T = 10;
+const V = 5;
+
+function calculateWindChill(temp, speed) {
+	return (
+		13.12 +
+		0.6215 * temp -
+		11.37 * Math.pow(speed, 0.16) +
+		0.3965 * temp * Math.pow(speed, 0.16)
+	);
+}
+
+const windChillOutput = document.getElementById("wind-chill");
+
+if (T <= 10 && V > 4.8) {
+	const windChillValue = calculateWindChill(T, V);
+
+	windChillOutput.textContent = windChillValue.toFixed(1) + " °C";
+} else {
+	windChillOutput.textContent = "N/A";
+}
